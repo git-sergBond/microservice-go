6b7402a21217d3cc05ff7a96678985efd7f704d1
git-sergBond <git-serg.bond@yandex.ru>
2022-09-12 18:07:31 +0300

    add http API for get current time

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
+
+type Time struct {
+	Time     string `json: "time,omitemty"`
+	NanoTime string `json: "nanoTime,omitemty"`
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World from Go and k8s")
-	})
+	log.Print("Begin working service time-service")
+
+	http.HandleFunc("/time", serveTime)
+	http.HandleFunc("/nanoTime", serveNanoTime)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
+}
+
+func serveTime(w http.ResponseWriter, r *http.Request) {
+	log.Print("serveTime() - start")
+	var serverTime Time
+	serverTime.Time = time.Now().String()
+	json.NewEncoder(w).Encode(serverTime)
+}
+
+func serveNanoTime(w http.ResponseWriter, r *http.Request) {
+	log.Print("serveNanoTime() - start")
+	var nanoTime Time
+	nanoTime.NanoTime = strconv.FormatInt(time.Now().UnixNano(), 10)
+	json.NewEncoder(w).Encode(nanoTime)
 }
